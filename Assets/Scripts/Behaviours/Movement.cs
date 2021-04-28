cc40d26a387df27697dacc76b3398e02b5bc0dd3
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-04-28 00:38:36 +0000

    Rewrite player movement to be more robust
    Moved away from using triggers in the animator in favour of bools that can be controlled and polled easily in code.
    
    Edited Idle animation to remove head rotation as this was too close to the No animation and detracted from the message conveyed to the player.

--- Assets/Scripts/Behaviours/Movement.cs
+++ Assets/Scripts/Behaviours/Movement.cs
@@ -1,78 +1,108 @@
-﻿using DG.Tweening;
+﻿using System;
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
-[RequireComponent(typeof(Animator))]
-public class Movement : MonoBehaviour
+namespace Behaviours
 {
-    public float duration = 1;
-    public LayerMask obstructionsLayer;
-
-    private PlayerInput playerInput;
-    private Animator animator;
-    private Tweener tweener;
-    private static readonly int Walk = Animator.StringToHash("Walk");
-    private static readonly int Stop = Animator.StringToHash("Stop");
-    private static readonly int No = Animator.StringToHash("No");
-
-    private void Awake()
+    [RequireComponent(typeof(PlayerInput))]
+    [RequireComponent(typeof(Animator))]
+    public class Movement : MonoBehaviour
     {
-        playerInput = GetComponent<PlayerInput>();
-        playerInput.SwitchCurrentControlScheme("Keyboard&Mouse", Keyboard.current);
+        public float duration = 1;
+        public LayerMask obstructionsLayer;
 
-        animator = GetComponent<Animator>();
-    }
+        private PlayerInput playerInput;
+        private Animator animator;
+        private static readonly int Walk = Animator.StringToHash("Walk");
+        private static readonly int Unable = Animator.StringToHash("Unable");
+        private const string MoveAction = "Move";
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        if (!context.action.triggered || tweener != null)
+        private void Awake()
         {
-            return;
+            playerInput = GetComponent<PlayerInput>();
+            playerInput.SwitchCurrentControlScheme("Keyboard&Mouse", Keyboard.current);
+
+            animator = GetComponent<Animator>();
         }
 
-        var input = context.ReadValue<Vector2>();
-
-        var position = transform.position;
-
-        var targetPosition = new Vector3(
-            position.x + input.x,
-            position.y,
-            position.z + input.y);
-
-        var heading = targetPosition - position;
-        var direction = heading / heading.magnitude;
-
-        var rayCastOrigin = new Vector3(position.x, position.y + 0.5f, position.z);
-
-        const int rayDistance = 1;
-        Debug.DrawRay(rayCastOrigin, direction, Color.red, 0.2f);
-        var hit = Physics.Raycast(rayCastOrigin, direction, rayDistance, obstructionsLayer);
-
-        var animInfo = animator.GetCurrentAnimatorStateInfo(0);
-        if (hit)
+        private void OnEnable()
         {
-            if (animInfo.tagHash != No)
+            playerInput.onActionTriggered += OnActionTriggered;
+        }
+
+        private void OnDisable()
+        {
+            playerInput.onActionTriggered -= OnActionTriggered;
+        }
+
+        private void OnActionTriggered(InputAction.CallbackContext context)
+        {
+            if (context.action.name == MoveAction)
             {
-                animator.SetTrigger(No);
+                OnMove(context);
+            }
+        }
+
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            if (!context.action.triggered || animator.GetBool(Walk))
+            {
+                return;
             }
 
-            return;
+            var input = context.ReadValue<Vector2>();
+
+            var position = transform.position;
+
+            var targetPosition = new Vector3(
+                position.x + input.x,
+                position.y,
+                position.z + input.y);
+
+            if (!CanMove(targetPosition))
+            {
+                if (animator.GetBool(Unable))
+                {
+                    return;
+                }
+
+                animator.SetBool(Unable, true);
+                StartCoroutine(Defer(() => animator.SetBool(Unable, false)));
+
+                return;
+            }
+
+            Move(targetPosition);
         }
 
-        Move(targetPosition);
-    }
+        private IEnumerator Defer(Action action, float delay = 0.1f)
+        {
+            yield return new WaitForSeconds(delay);
+            action();
+        }
 
-    private void Move(Vector3 targetPosition)
-    {
-        tweener = transform.DOMove(targetPosition, duration)
-            .OnStart(() => { animator.SetTrigger(Walk); })
-            .OnComplete(() =>
-            {
-                animator.SetTrigger(Stop);
-                tweener = null;
-            });
+        private bool CanMove(Vector3 targetPosition)
+        {
+            var position = transform.position;
+            var heading = targetPosition - position;
+            var direction = heading / heading.magnitude;
 
-        transform.DOLookAt(targetPosition, duration);
+            var rayCastOrigin = new Vector3(position.x, position.y + 0.5f, position.z);
+
+            const int rayDistance = 1;
+            Debug.DrawRay(rayCastOrigin, direction, Color.red, 0.2f);
+            return !Physics.Raycast(rayCastOrigin, direction, rayDistance, obstructionsLayer);
+        }
+
+        private void Move(Vector3 targetPosition)
+        {
+            transform.DOMove(targetPosition, duration)
+                .OnStart(() => animator.SetBool(Walk, true))
+                .OnComplete(() => animator.SetBool(Walk, false));
+
+            transform.DOLookAt(targetPosition, duration);
+        }
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -41,6 +41,14 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""5e02da27-fe95-4202-8dd5-2c6edc9351c3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -261,6 +269,28 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""processors"": """",
                     ""groups"": ""XR"",
                     ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c6b409ee-841b-4fcd-ba95-197d89182ad4"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""26af9457-d7a4-4298-9445-58d70191bd5c"",
+                    ""path"": ""*/{PrimaryAction}"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Interact"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -841,6 +871,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -905,6 +936,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Interact;
     public struct PlayerActions
     {
         private @PlayerControls m_Wrapper;
@@ -912,6 +944,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Interact => m_Wrapper.m_Player_Interact;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -930,6 +963,9 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                 @Fire.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
                 @Fire.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
                 @Fire.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
+                @Interact.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteract;
+                @Interact.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteract;
+                @Interact.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteract;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -943,6 +979,9 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                 @Fire.started += instance.OnFire;
                 @Fire.performed += instance.OnFire;
                 @Fire.canceled += instance.OnFire;
+                @Interact.started += instance.OnInteract;
+                @Interact.performed += instance.OnInteract;
+                @Interact.canceled += instance.OnInteract;
             }
         }
     }
@@ -1102,6 +1141,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
+        void OnInteract(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
