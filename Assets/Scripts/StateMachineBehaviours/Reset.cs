ffccce134a9d4e43442e18a78effe162cc93fecf
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-04-28 09:13:35 +0000

    Add Reset state to be able to trigger a reset of state machine parameters to their default values

---
+++ Assets/Scripts/StateMachineBehaviours/Reset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace StateMachineBehaviours
+{
+    public class Reset : StateMachineBehaviour
+    {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.WriteDefaultValues();
+        }
+    }
+}
