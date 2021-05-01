132a27952fc8a1b2db0346c1977f4fb242d295a9
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-05-01 22:33:51 +0000

    Add glowing level goal

---
+++ Assets/Scripts/Behaviours/Win.cs
@@ -0,0 +1,27 @@
+﻿using StateMachineBehaviours;
+using UnityEngine;
+
+namespace Behaviours
+{
+    public class Win : MonoBehaviour
+    {
+        private GameObject player;
+        private Animator animator;
+
+        private void Awake()
+        {
+            player = GameObject.FindWithTag("Player");
+            animator = player.GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject != player)
+            {
+                return;
+            }
+
+            animator.SetTrigger((int)RobotAnimatorParameter.Win);
+        }
+    }
+}
--- Assets/Scripts/StateMachineBehaviours/RobotAnimatorParameter.cs
+++ Assets/Scripts/StateMachineBehaviours/RobotAnimatorParameter.cs
@@ -3,8 +3,8 @@
     public enum RobotAnimatorParameter
     {
         Walk = 765711723,
-        Punch = -316645540,
         Unable = -136654945,
         Dead = 1293411866,
+        Win = -2004386410
     }
 }
