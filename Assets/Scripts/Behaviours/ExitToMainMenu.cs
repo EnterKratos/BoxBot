e0aaeba414aeb477747014d4a4ae608c27d31f1a
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-05-09 12:22:23 +0000

    Fix ExitToMainMenu regression
    A previous change to the PlayerInput component to use Unity Events instead of C# Events rendered the ExitToMainMenu behaviour inert.
    The fix was to strip it back to just the handler function and wire it up in the UI.

--- Assets/Scripts/Behaviours/ExitToMainMenu.cs
+++ Assets/Scripts/Behaviours/ExitToMainMenu.cs
@@ -4,40 +4,16 @@ using UnityEngine.SceneManagement;
 
 namespace Behaviours
 {
-    [RequireComponent(typeof(PlayerInput))]
     public class ExitToMainMenu : MonoBehaviour
     {
-        private PlayerInput playerInput;
-        private const string Exit = "Exit";
-
-        private void Awake()
-        {
-            playerInput = GetComponent<PlayerInput>();
-        }
-
-        private void OnEnable()
-        {
-            playerInput.onActionTriggered += OnActionTriggered;
-        }
-
-        private void OnDisable()
-        {
-            playerInput.onActionTriggered -= OnActionTriggered;
-        }
-
-        private void OnActionTriggered(InputAction.CallbackContext context)
+        public void OnExit(InputAction.CallbackContext context)
         {
             if (!context.action.triggered)
             {
                 return;
             }
 
-            switch (context.action.name)
-            {
-                case Exit:
-                    SceneManager.LoadScene(0);
-                    break;
-            }
+            SceneManager.LoadScene(0);
         }
     }
 }
