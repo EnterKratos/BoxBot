2971db8b729299d7859d2ff397c2acfc608b38b9
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-05-09 14:56:09 +0000

    Add game over menu

---
+++ Assets/Scripts/Behaviours/DeathUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Behaviours
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class DeathUI : MonoBehaviour
+    {
+        public GameObject ui;
+
+        private PlayerInput playerInput;
+
+        private void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+        }
+
+        public void DisplayUI()
+        {
+            StartCoroutine(DisplayUICoroutine());
+        }
+
+        private IEnumerator DisplayUICoroutine()
+        {
+            // Waiting because enabling the TMP component seems to cause a stutter
+            yield return new WaitForSeconds(1f);
+
+            ui.SetActive(true);
+            playerInput.SwitchCurrentActionMap("UI");
+        }
+    }
+}
