f50802f57ed1198e8280d5681e96ad2a866f8f70
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-05-12 10:19:49 +0000

    Add audio to death menu

--- Assets/Scripts/Behaviours/ExitToMainMenu.cs
+++ Assets/Scripts/Behaviours/ExitToMainMenu.cs
@@ -6,12 +6,22 @@ namespace Behaviours
 {
     public class ExitToMainMenu : MonoBehaviour
     {
+        public new AudioClip audio;
+        private AudioController audioController;
+
+        private void Start()
+        {
+            audioController = FindObjectOfType<AudioController>();
+        }
+
         public void OnExit(InputAction.CallbackContext context)
         {
-            if (!context.action.triggered)
+            if (!context.performed)
             {
                 return;
             }
+
+            audioController.Play(audio);
 
             SceneManager.LoadScene((int)Scene.MainMenu);
         }
--- Assets/Scripts/Behaviours/LevelReset.cs
+++ Assets/Scripts/Behaviours/LevelReset.cs
@@ -6,12 +6,22 @@ namespace Behaviours
 {
     public class LevelReset : MonoBehaviour
     {
+        public new AudioClip audio;
+        private AudioController audioController;
+
+        private void Start()
+        {
+            audioController = FindObjectOfType<AudioController>();
+        }
+
         public void OnReset(InputAction.CallbackContext context)
         {
             if (!context.performed)
             {
                 return;
             }
+
+            audioController.Play(audio);
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
