ed5ee9822991ccc86401ab461bff9d7ee66dde1e
Enter_Kratos <15807441+EnterKratos@users.noreply.github.com>
2021-05-23 17:45:14 +0000

    Disable cursor on preload

---
+++ Assets/Scripts/Behaviours/HideCursorOnAwake.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Behaviours
+{
+    public class HideCursorOnAwake : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Cursor.visible = false;
+        }
+    }
+}
